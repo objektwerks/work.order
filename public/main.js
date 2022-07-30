fe49bcc50df0c4eb031e7a8e1c8802267a6ee932
objektwerks <objektwerks@runbox.com>
2022-07-30 13:40:06 -0400

    edited main.js

--- public/main.js
+++ public/main.js
@@ -1,5 +1,9 @@
+import store from './store.js';
+
 export default () => {
     const ADMIN = 'admin';
     const HOMEOWNER = 'homeowner';
     const LAWNCARE_PROVIDER = 'lawncare provider';
+
+    store();
 };
