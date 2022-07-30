6386934c38e5f5a2eeb11b77b4ee8d759fac88e8
objektwerks <objektwerks@runbox.com>
2022-07-30 17:01:28 -0400

    edited server.js

--- server.js
+++ server.js
@@ -1,13 +1,9 @@
-import store from './store.js';
-
-store();
-
 var host = "127.0.0.1";
 var port = 1337;
 var express = require("express");
 
 var server = express();
-server.use(express.static('public'))
+server.use( express.static('public') )
 server.listen(port, host);
 
 console.log('Running server at http://localhost:' + port + '/');
