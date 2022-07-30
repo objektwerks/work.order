c94b2ad37a1b2dc59fbace1812f2abc400642634
objektwerks <objektwerks@runbox.com>
2022-07-30 09:15:35 -0400

    edited ddl.sql

--- ddl.sql
+++ ddl.sql
@@ -1,22 +1,14 @@
-create table if not exists admin (
-  email_address varchar not null
-);
-create index if not exists admin_email_address_idx ON admin(email_address);
-
-create table if not exists homeowner (
+create table if not exists user (
+  name varchart not null,
   email_address varchar not null,
-  street_address varchar not null
+  street_address varchar not null,
+  role varchar not null
 );
-create index if not exists homeowner_email_address_idx ON homeowner(email_address);
-
-create table if not exists lawncare_provider (
-  email_address varchar not null
-);
-create index if not exists user_email_address_idx ON lawncare_provider(email_address);
+create index if not exists user_email_address_idx ON user(email_address);
 
 create table if not exists work_order (
-  homeowner varchar not null,
-  lawncare_provider varchar not null,
+  homeowner_email_address varchar not null,
+  lawncare_provider_email_address varchar not null,
   opened datetime not null,
   issue varchar not null,
   resolution varchar not null,
