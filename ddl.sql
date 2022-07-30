d5f349ebf6813973a2a93327bd2f723c6c61ef98
objektwerks <objektwerks@runbox.com>
2022-07-30 12:48:46 -0400

    edited ddl.sql

--- ddl.sql
+++ ddl.sql
@@ -3,19 +3,19 @@ create database if not exists `work_order_db`;
 use `work_order_db`
 
 create table if not exists `user` (
-  `name` tinytext not null,
-  `role` tinytext not null,
-  `email_address` tinytext not null,
-  `street_address` tinytext not null,
+  `name` text(64) not null,
+  `role` text(64) not null,
+  `email_address` text(128) not null,
+  `street_address` text(128) not null,
   primary key (`name`, `email_address`)
 );
 
 create table if not exists `work_order` (
   `number` bigint not null auto_increment,
-  `homeowner_name` tinytext not null,
-  `homeowner_email_address` tinytext not null,
-  `lawncare_provider_name` tinytext not null,
-  `lawncare_provider_email_address` tinytext not null,
+  `homeowner_name` text(64) not null,
+  `homeowner_email_address` text(64) not null,
+  `lawncare_provider_name` text(64) not null,
+  `lawncare_provider_email_address` text(64) not null,
   `opened` datetime not null,
   `issue` text(1028) not null,
   `resolution` text(1028) not null,
@@ -24,7 +24,7 @@ create table if not exists `work_order` (
   constraint homeowner_name_fk foreign key (`homeowner_name`) references user(`name`)
   constraint homeowner_email_adddress_fk foreign key (`homeowner_email_address`) references user(`email_address`)
   constraint lawncare_provider_name_fk foreign key (`lawncare_provider_name`) references user(`name`)
-  constraint lawncare_provider_name_fk foreign key (`lawncare_provider_email_address`) references user(`email_address`)
+  constraint lawncare_provider_email_address_fk foreign key (`lawncare_provider_email_address`) references user(`email_address`)
 );
 create index if not exists work_order_homeowner_name_idx ON work_order(`homeowner_name`);
 create index if not exists work_order_homeowner_email_address_idx ON work_order(`homeowner_email_address`);
