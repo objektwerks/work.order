143d27394f4c83c90740470b0bb91fee6a848c90
objektwerks <objektwerks@runbox.com>
2022-07-30 13:05:41 -0400

    edited ddl.sql

--- ddl.sql
+++ ddl.sql
@@ -1,18 +1,20 @@
-create database if not exists `work_order_db`;
+drop database if exists `work_order_db`;
+
+create database `work_order_db`;
 
 use `work_order_db`
 
-create table if not exists `user` (
+create table `user` (
   `name` varchar(128) not null,
   `role` varchar(64) not null,
   `email_address` varchar(128) not null,
   `street_address` varchar(255) not null,
   primary key (`name`, `email_address`)
 );
-create index if not exists user_name_idx ON `user`(`name`);
-create index if not exists user_email_address_idx ON `user`(`email_address`);
+create index user_name_idx ON `user`(`name`);
+create index user_email_address_idx ON `user`(`email_address`);
 
-create table if not exists `work_order` (
+create table `work_order` (
   `number` bigint not null auto_increment,
   `homeowner_name` varchar(128) not null,
   `homeowner_email_address` varchar(128) not null,
@@ -28,6 +30,6 @@ create table if not exists `work_order` (
   constraint lawncare_provider_name_fk foreign key (`lawncare_provider_name`) references `user`(`name`),
   constraint lawncare_provider_email_address_fk foreign key (`lawncare_provider_email_address`) references `user`(`email_address`)
 );
-create index if not exists work_order_homeowner_name_idx ON `work_order`(`homeowner_name`);
-create index if not exists work_order_homeowner_email_address_idx ON `work_order`(`homeowner_email_address`);
-create index if not exists work_order_opened_idx ON `work_order`(`opened`);
+create index work_order_homeowner_name_idx ON `work_order`(`homeowner_name`);
+create index work_order_homeowner_email_address_idx ON `work_order`(`homeowner_email_address`);
+create index work_order_opened_idx ON `work_order`(`opened`);
