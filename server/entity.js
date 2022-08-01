95aeb87d1dd2be9661a389dd47fe8871eae5344d
objektwerks <objektwerks@runbox.com>
2022-08-01 12:41:28 -0400

    added user to entity

--- server/entity.js
+++ server/entity.js
@@ -1,3 +1,13 @@
 export const admin = "admin";
 export const homeowner = "homeowner";
 export const serviceProvider = "serviceprovider"
+
+export class User {
+  constructor(id, role, name, emailAddress, streetAddress, registered) {
+    this.id = id;
+    this.role = role;
+    this.emailAddress = emailAddress;
+    this.streetAddress = streetAddress;
+    this.registered = registered;
+  }
+}
