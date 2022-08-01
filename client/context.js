897ab5178c02fc3f9ccf1655485cb94b9ffa5d5a
objektwerks <objektwerks@runbox.com>
2022-08-01 12:43:55 -0400

    edited context

--- client/context.js
+++ client/context.js
@@ -1,3 +1,25 @@
 export const admin = "admin";
 export const homeowner = "homeowner";
 export const serviceProvider = "serviceprovider"
+
+export class User {
+  constructor(id, role, name, emailAddress, streetAddress, registered) {
+    this.id = id;
+    this.role = role;
+    this.emailAddress = emailAddress;
+    this.streetAddress = streetAddress;
+    this.registered = registered;
+  }
+}
+
+export class WorkOrder {
+  constructor(number, homeowner, serviceProvider, opened, closed, issue, resolution) {
+    this.number = number;
+    this.homeowner = homeowner;
+    this.serviceProvider = serviceProvider;
+    this.opened = opened;
+    this.closed = closed;
+    this.issue = issue;
+    this.resolution = resolution;
+  }
+}
