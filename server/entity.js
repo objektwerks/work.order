05f407a6202a2d22214c58eb44585c670ec62db7
objektwerks <objektwerks@runbox.com>
2022-08-01 14:47:43 -0400

    added as json to context and entity

--- server/entity.js
+++ server/entity.js
@@ -10,16 +10,42 @@ export class User {
     this.streetAddress = streetAddress;
     this.registered = registered;
   }
+
+  asJson() {
+    return JSON.stringify(
+      {
+        id: this.id,
+        role: this.role,
+        emmailAddress: this.emailAddress,
+        streetAddress: this.streetAddress,
+        registered: this.registered
+      }
+    )
+  }
 }
 
 export class WorkOrder {
   constructor(number, homeowner, serviceProvider, opened, closed, issue, resolution) {
-    this.number = number;
-    this.homeowner = homeowner;
-    this.serviceProvider = serviceProvider;
-    this.opened = opened;
-    this.closed = closed;
-    this.issue = issue;
-    this.resolution = resolution;
+    this.number = number,
+    this.homeowner = homeowner,
+    this.serviceProvider = serviceProvider,
+    this.opened = opened,
+    this.closed = closed,
+    this.issue = issue,
+    this.resolution = resolution
+  }
+
+  asJson() {
+    return JSON.stringify(
+      {
+        number: this.number,
+        homeowner: this.homeowner,
+        serviceProvider: this.serviceProvider,
+        opened: this.opened,
+        closed: this.closed,
+        issue: this.issue,
+        resolution: this.resolution
+      }
+    )
   }
 }
