a32bbbc12b20b54e5f98f26329253a6c35bb71c1
objektwerks <objektwerks@runbox.com>
2022-08-01 06:40:05 -0400

    edited store.js

--- server/store.js
+++ server/store.js
@@ -2,7 +2,7 @@ import mysql from 'mysql';
 
 const ADMIN = 'admin';
 const HOMEOWNER = 'homeowner';
-const LAWNCARE_PROVIDER = 'lawncare provider';
+const SERVICE_PROVIDER = 'service provider';
 
 const url = process.env.DATABASE_URL;
 const connection = mysql.createConnection(url);
