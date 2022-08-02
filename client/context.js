a6320bcf4c34ee62c10eb8e33df5084e3ca9a166
objektwerks <objektwerks@runbox.com>
2022-08-02 11:58:23 -0400

    enhanced random pwd

--- client/context.js
+++ client/context.js
@@ -7,7 +7,7 @@ const letters = "abcdefghijklmnopqrstuvwxyz";
 const shuffle = str => [...str].sort( () => Math.random()-.5 ).join('');
 export const randomPassword = () => {
   const array = new Uint16Array(7);
-  const numbers = crypto.getRandomValues(array)[0].toString();
+  const numbers = crypto.getRandomValues(array)[0].toString().slice(0, 3);
   const first = shuffle(specialChars).charAt(0);
   const left = shuffle(letters).charAt(0);
   const right = shuffle(letters).charAt(0);
