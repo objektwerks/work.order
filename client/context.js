a78f58c3b78d11b24f17f8b8c86c4bff32b3f20b
objektwerks <objektwerks@runbox.com>
2022-08-01 20:00:29 -0400

    refactored entity and context

--- client/context.js
+++ client/context.js
@@ -2,68 +2,47 @@ export const admin = "admin";
 export const homeowner = "homeowner";
 export const serviceProvider = "serviceprovider"
 
-export class User {
-  constructor(id, role, name, emailAddress, streetAddress) {
-    this(id, role, name, emailAddress, streetAddress, new Date().toISOString)
+export class Entity {
+  static toJson(object) {
+    return JSON.stringify(object)
   }
 
-  constructor(id, role, name, emailAddress, streetAddress, registered) {
-    this.id = id;
-    this.role = role;
-    this.name = name;
-    this.emailAddress = emailAddress;
-    this.streetAddress = streetAddress;
-    this.registered = registered;
-  }
-
-  toJson() {
-    return JSON.stringify(
-      {
-        id: this.id,
-        role: this.role,
-        name: this.name,
-        emmailAddress: this.emailAddress,
-        streetAddress: this.streetAddress,
-        registered: this.registered
-      }
-    )
-  }
-
-  toObject(json) {
+  static toObject(json) {
     return JSON.parse(json)
   }
 }
 
+export class User {
+  static create(id, role, name, emailAddress, streetAddress) {
+    return create(id, role, name, emailAddress, streetAddress, new Date().toISOString);
+  }
+
+  static create(id, role, name, emailAddress, streetAddress, registered) {
+    return {
+      id: id,
+      role: role,
+      name: name,
+      emailAddress: emailAddress,
+      streetAddress: streetAddress,
+      registered: registered
+    }
+  }
+}
+
 export class WorkOrder {
-  constructor(number, homeownerId, serviceProviderId, issue) {
-    this(number, homeownerId, serviceProviderId, issue, "", new Date().toISOString, "")
+  static create(number, homeownerId, serviceProviderId, issue) {
+    return create(number, homeownerId, serviceProviderId, issue, "", new Date().toISOString, "");
   }
 
-  constructor(number, homeownerId, serviceProviderId, issue, resolution, opened, closed) {
-    this.number = number,
-    this.homeownerId = homeownerId,
-    this.serviceProviderId = serviceProviderId,
-    this.issue = issue,
-    this.resolution = resolution,
-    this.opened = opened,
-    this.closed = closed
-  }
-
-  toJson() {
-    return JSON.stringify(
-      {
-        number: this.number,
-        homeownerId: this.homeownerId,
-        serviceProviderId: this.serviceProviderId,
-        issue: this.issue,
-        resolution: this.resolution,
-        opened: this.opened,
-        closed: this.closed
-      }
-    )
-  }
-
-  toObject(json) {
-    return JSON.parse(json)
+  static create(number, homeownerId, serviceProviderId, issue, resolution, opened, closed) {
+    return {
+      number: number,
+      homeownerId: homeownerId,
+      serviceProviderId: serviceProviderId,
+      issue: issue,
+      resolution: resolution,
+      opened: opened,
+      closed: closed
+    }
   }
 }
