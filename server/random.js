d6d14ea138387ddb6fa860d00e950fb11c784368
objektwerks <objektwerks@runbox.com>
2022-08-02 12:52:18 -0400

    enhanced random

--- server/random.js
+++ server/random.js
@@ -11,12 +11,12 @@ const letters = "abcdefghijklmnopqrstuvwxyz";
 
 const shuffle = str => [...str].sort( () => Math.random() - .5 ).join('');
 
-export function password() {
+export function pin() {
   const array = new Uint16Array(7);
   const numbers = crypto.getRandomValues(array)[0].toString().slice(0, 3);
   const first = shuffle(specialChars).charAt(0);
   const left = shuffle(letters).charAt(0);
-  const right = shuffle(letters).charAt(0);
+  const right = shuffle(letters).charAt(letters.length - 1);
   const last = shuffle(specialChars).charAt(specialChars.length - 1);
   const password = first + left + numbers + right + last;
   return password;
