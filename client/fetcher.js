94b7ba5f6ff7d14be714f1240d3ece435754f120
objektwerks <objektwerks@runbox.com>
2022-08-05 08:05:19 -0400

    edited fetcher

--- client/fetcher.js
+++ client/fetcher.js
@@ -1,25 +1,4 @@
 // @ts-check
-
-/*
-2. post - /register
-3. post - /login
-4. post - /workorders/add
-5. post - /workorders/update
-6. get  - /workorders/:userid
-7. get  - /workorders/:number
-8. post - /users/update
-9. get  - /users/:role
-
-1. client:register --- registration --> server --- status ---> client
-2. client:login --- credentials --> server --- user, workorders --> client
-3. client:add --- work order --> server --- workorder --> client
-4. client:update --- work order --> server --- status --> client
-5. client:list --- userid --> server --- workorders --> client
-6. client:get --- number --> server --- workorder --> client
-7. client:update --- user --> server --- status --> client
-8. client:list --- role --> server --- users --> client
-*/
-
 import { toJson, toObject, Status, User, Users, UserWorkOrders, WorkOrder, WorkOrders } from './model.js';
 
 export class Fetcher {
