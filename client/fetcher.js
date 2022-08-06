4b249370c98bf10e5a1c46a3409c48d23c4805bd
objektwerks <objektwerks@runbox.com>
2022-08-06 14:02:22 -0400

    refactored fetcher

--- client/fetcher.js
+++ client/fetcher.js
@@ -20,7 +20,7 @@ export default class Fetcher {
     }
   }
 
-  async send(url, method, entity, fault) {
+  async fetch(url, method, entity, fault) {
     return await fetch(url, {
       method: method,
       headers: this.headers,
@@ -35,34 +35,34 @@ export default class Fetcher {
   }
 
   async register(registration) {
-    return await this.send(this.registerUrl, this.post, registration, () => Status.fail('Register failed.'));
+    return await this.fetch(this.registerUrl, this.post, registration, () => Status.fail('Register failed.'));
   }
 
   async login(credentials) {
-    return await this.send(this.loginUrl, this.post, credentials, () => UserWorkOrders.fail('Login failed.'));
+    return await this.fetch(this.loginUrl, this.post, credentials, () => UserWorkOrders.fail('Login failed.'));
   }
 
   async addWorkOrder(workorder) {
-    return await this.send(this.addWorkOrderUrl, this.post, workorder, () => WorkOrder.fail('Add work order failed!'));
+    return await this.fetch(this.addWorkOrderUrl, this.post, workorder, () => WorkOrder.fail('Add work order failed!'));
   }
 
   async updateWorkOrder(workorder) {
-    return await this.send(this.updateWorkOrderUrl, this.post, workorder, () => WorkOrder.fail('Update work order failed!'));
+    return await this.fetch(this.updateWorkOrderUrl, this.post, workorder, () => WorkOrder.fail('Update work order failed!'));
   }
 
   async getWorkOrderByNumber(number) {
-    return await this.send(this.getWorkOrderByNumberUrl + number, this.get, {}, () => WorkOrder.fail('Get work order by number failed!'));
+    return await this.fetch(this.getWorkOrderByNumberUrl + number, this.get, {}, () => WorkOrder.fail('Get work order by number failed!'));
   }
 
   async listWorkOrdersByUserId(id) {
-    return await this.send(this.listWorkOrdersByUserIdUrl + id, this.get, {}, () => WorkOrders.fail('List work orders by user failed!'));
+    return await this.fetch(this.listWorkOrdersByUserIdUrl + id, this.get, {}, () => WorkOrders.fail('List work orders by user failed!'));
   }
 
   async updateUser(user) {
-    return await this.send(this.updateUserUrl, this.post, user, () => Status.fail('Update user failed.'));
+    return await this.fetch(this.updateUserUrl, this.post, user, () => Status.fail('Update user failed.'));
   }
 
   async listUsersByRole(role) {
-    return await this.send(this.listUsersByRoleUrl + role, this.get, {}, () => Users.fail('List users by role failed!'));
+    return await this.fetch(this.listUsersByRoleUrl + role, this.get, {}, () => Users.fail('List users by role failed!'));
   }
 }
