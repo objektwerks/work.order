75e978dc1a0720b9bc30138bd86dd78fb485ed5b
objektwerks <objektwerks@runbox.com>
2022-08-08 19:26:38 -0400

    added register.js; refactored client

--- client/client.js
+++ client/client.js
@@ -2,22 +2,11 @@
 import Menu from './menu.js';
 import Fetcher from './fetcher.js';
 import Login from './login.js';
+import Register from './register.js';
 
-const host = '127.0.0.1';
-const port = 3000;
-const fetcher = new Fetcher(`http://${host}:${port}`);
-
+const fetcher = new Fetcher(`http://127.0.0.1:3000`);
 const menu = new Menu();
-
-const login = new Login();
+const login = new Login(fetcher);
+const register = new Register(fetcher);
 
 console.log('*** client running ...');
-
-// Register
-document.getElementById('register-command-id').addEventListener('click', function() {
-  // get form values
-  // validate form values
-  // package form values
-  // call fetcher
-  // handle fetcher return value vis-a-vis UI
-}, false);
---
+++ client/register.js
@@ -0,0 +1,11 @@
+export default class Register {
+  constructor(fetcher) {
+    document.getElementById('register-command-id').addEventListener('click', () => {
+      // get form values
+      // validate form values
+      // package form values
+      // call fetcher
+      // handle fetcher return value vis-a-vis UI
+    }, false);
+  }
+}
