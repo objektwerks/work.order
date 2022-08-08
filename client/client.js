e0a71dad0bb1b39b21decac7ff5e5bd0bc39a105
objektwerks <objektwerks@runbox.com>
2022-08-08 07:56:51 -0400

    add function to client

--- client/client.js
+++ client/client.js
@@ -6,3 +6,11 @@ const port = 3000;
 const fetcher = new Fetcher(`http://${host}:${port}`);
 
 console.log('*** client running ...');
+
+function selectLoginRegisterTab(tabId) {
+  let tabs = document.getElementsByClassName("login-register-tab");
+  for (const tab of tabs) {
+    tab["style"].display = "none";
+  }
+  document.getElementById(tabId).style.display = "block";
+}
