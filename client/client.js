79cd42afec8770f621d16b436a9e02096b8abe8b
objektwerks <objektwerks@runbox.com>
2022-08-08 08:33:21 -0400

    edited index.html and client

--- client/client.js
+++ client/client.js
@@ -8,11 +8,11 @@ const fetcher = new Fetcher(`http://${host}:${port}`);
 console.log('*** client running ...');
 
 function selectLoginRegisterTab(tabId) {
-  let tabs = document.getElementsByClassName("login-register-tab");
+  let tabs = document.getElementsByClassName('login-register-tab');
   for (const tab of tabs) {
-    tab["style"].display = "none";
+    tab['style'].display = 'none';
   }
-  document.getElementById(tabId).style.display = "block";
+  document.getElementById(tabId).style.display = 'block';
 }
 
 document.getElementById('login-button-id').addEventListener('click', function() {
