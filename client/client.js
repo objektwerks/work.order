9b1f1ff9d73ecd4e8c0bffc93f7d4ed8067131d1
objektwerks <objektwerks@runbox.com>
2022-08-08 19:22:28 -0400

    added login.js; refactored client

--- client/client.js
+++ client/client.js
@@ -1,6 +1,7 @@
 // @ts-check
 import Menu from './menu.js';
 import Fetcher from './fetcher.js';
+import Login from './login.js';
 
 const host = '127.0.0.1';
 const port = 3000;
@@ -8,17 +9,9 @@ const fetcher = new Fetcher(`http://${host}:${port}`);
 
 const menu = new Menu();
 
+const login = new Login();
+
 console.log('*** client running ...');
-
-
-// Login
-document.getElementById('login-command-id').addEventListener('click', function() {
-  // get form values
-  // validate form values
-  // package form values
-  // call fetcher
-  // handle fetcher return value vis-a-vis UI
-}, false);
 
 // Register
 document.getElementById('register-command-id').addEventListener('click', function() {
---
+++ client/login.js
@@ -0,0 +1,11 @@
+export default class Login {
+  constructor(fetcher) {
+    document.getElementById('login-command-id').addEventListener('click', () => {
+      // get form values
+      // validate form values
+      // package form values
+      // call fetcher
+      // handle fetcher return value vis-a-vis UI
+    }, false);
+  }
+}
