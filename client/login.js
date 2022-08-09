2ad2511f70e04cb12089cf0a0487a8eb04b20677
objektwerks <objektwerks@runbox.com>
2022-08-09 07:00:49 -0400

    implemented listener in login

--- client/login.js
+++ client/login.js
@@ -1,14 +1,24 @@
 // @ts-check
+import { Credentials } from './model.js';
+
 export default class Login {
   constructor(fetcher) {
+    this.fetcher = fetcher;
+
     document.getElementById('login-command-id').addEventListener('click', () => {
-      // get form values
-      const emailAddress = document.getElementById('login-email-address-id').value;
-      const pin = document.getElementById('login-pin-id').value;
-      // validate form values
-      // package form values
-      // call fetcher
-      // handle fetcher return value vis-a-vis UI
+      const emailAddress =  document.getElementById('login-email-address-id')['value'];
+      const pin = document.getElementById('login-pin-id')['value']
+
+      Credentials.validate(emailAddress, pin);
+      const credentials = Credentials.create(emailAddress, pin);
+
+      const userWorkOrders = this.fetch(credentials);
+      console.log('login -> userWorkOrders', userWorkOrders);
+      // TODO: Set UI
     }, false);
+  }
+
+  async fetch(credentials) {
+    return await this.fetcher.login(credentials);
   }
 }
