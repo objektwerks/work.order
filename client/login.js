51761ba30df2d68675511cf3415550d025b63705
objektwerks <objektwerks@runbox.com>
2022-08-09 06:40:05 -0400

    enhanced login

--- client/login.js
+++ client/login.js
@@ -1,7 +1,10 @@
+// @ts-check
 export default class Login {
   constructor(fetcher) {
     document.getElementById('login-command-id').addEventListener('click', () => {
       // get form values
+      const emailAddress = document.getElementById('login-email-address-id').value;
+      const pin = document.getElementById('login-pin-id').value;
       // validate form values
       // package form values
       // call fetcher
