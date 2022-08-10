f693ccaf55e98c2fc730042892f8c9583710f75e
objektwerks <objektwerks@runbox.com>
2022-08-10 08:53:58 -0400

    added event listener templates to workorders pane

--- client/workorders.js
+++ client/workorders.js
@@ -1,7 +1,20 @@
 // @ts-check
+import { getById } from './document.js';
 
 export default class WorkOrdersPane {
   constructor(fetcher) {
     this.fetcher = fetcher;
+
+    getById('workorder-new-command-id').addEventListener('click', () => {
+
+    }, false);
+
+    getById('workorder-save-command-id').addEventListener('click', () => {
+
+    }, false);
+
+    getById('workorders-list-command-id').addEventListener('click', () => {
+
+    }, false);
   }
 }
