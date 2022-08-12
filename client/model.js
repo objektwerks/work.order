0632a4d65b332ae505dc35351197a3f904a84900
objektwerks <objektwerks@runbox.com>
2022-08-12 11:23:30 -0400

    added user binding to model

--- client/model.js
+++ client/model.js
@@ -11,6 +11,10 @@ export default class Model {
   bindUserToUserView(user) {
     this.user = user;
     setValueById('user-role-id', user.role);
+    setValueById('user-name-id', user.name);
+    setValueById('user-email-address-id', user.emailAddress);
+    setValueById('user-street-address-id', user.streetAddress);
+    setValueById('user-registered-id', user.registered);
   }
 
   bindUserViewToUser(name, emailAddress, streetAddress) {
