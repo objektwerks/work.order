0860bd9767542eedc33f36a038edb671703cdc04
objektwerks <objektwerks@runbox.com>
2022-08-12 10:40:32 -0400

    enhanced user

--- client/user.js
+++ client/user.js
@@ -2,6 +2,9 @@
 import { getById, getValueById, hide, listValues, show } from './document.js';
 
 // @ts-ignore
+import { validateUser } from './validator.js';
+
+// @ts-ignore
 import { User } from './entity.js';
 
 export default class UserView {
@@ -16,7 +19,32 @@ export default class UserView {
       const name = getValueById('user-name-id');
       const emailAddress = getValueById('user-email-address-id');
       const streetAddress = getValueById('user-street-address-id');
+      const errors = validateUser(role, name, emailAddress, streetAddress);
       
+      if (errors.length === 0) {
+        const registration = User.create(role, name, emailAddress, streetAddress);
+        const status = this.fetch(registration);
+        if (!status.success) {
+          errors.push(status.error);
+          this.listErrors(errors);
+        } else {
+          hide('register-view-id"');
+          hide('register-menu-id');
+          
+          show('register-info-view-id');
+        }
+      } else {
+        this.listErrors(errors);
+      }      
     }, false);
+  }
+
+  fetch(registration) {
+    return this.fetcher.register(registration);
+  }
+
+  listErrors(errors) {
+    listValues('register-errors-list-id', errors);
+    show('register-errors-view-id');
   }
 }
