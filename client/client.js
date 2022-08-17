7333ad2e67a33d2d170b729143fa081a13410a43
objektwerks <objektwerks@runbox.com>
2022-08-17 17:30:24 -0400

    edited client

--- client/client.js
+++ client/client.js
@@ -1,7 +1,7 @@
 // @ts-check
+import Menu from './menu.js';
 import Fetcher from './fetcher.js';
 import Model from './model.js';
-import Menu from './menu.js';
 import LoginView from './login.view.js';
 import RegisterView from './register.view.js';
 import WorkOrdersView from './workorders.view.js';
@@ -9,9 +9,9 @@ import UserView from './user.view.js';
 
 export default class Client {
   constructor() {
+    this.menu = new Menu();
     this.fetcher = new Fetcher();
     this.model = new Model();
-    this.menu = new Menu();
     this.loginView = new LoginView(this.fetcher, this.model);
     this.registerView = new RegisterView(this.fetcher);
     this.workordersView = new WorkOrdersView(this.fetcher, this.model);
