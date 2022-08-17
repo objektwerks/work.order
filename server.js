3b778cddb6ebd87ec38fef526c47512b4d5cff66
objektwerks <objektwerks@runbox.com>
2022-08-17 19:32:00 -0400

    refactored emailer; edited server

--- server.js
+++ server.js
@@ -1,17 +1,11 @@
 // @ts-check
 import store from './server/store.js';
-import Emailer from './server/emailer.js';
+import emailer from './server/emailer.js';
 import Service from './server/service.js';
 import Router from './server/router.js';
 
 store();
-
-const host = process.env.EMAIL_HOST;
-const port = process.env.EMAIL_PORT;
-const sender = process.env.EMAIL_SENDER;
-const password = process.env.EMAIL_PASSWORD;
-const emailer = new Emailer(host, port, sender, password);
+emailer();
 
 const service = new Service(store, emailer);
-
 const router = new Router(service);
--- server/emailer.js
+++ server/emailer.js
@@ -1,38 +1,37 @@
 // @ts-check
 import nodemailer from 'nodemailer';
 
-export default class Emailer {
-  constructor(host, port, sender, password) {
-    this.host = host;
-    this.port = port;
-    this.sender = sender;
-    this.password = password;
-    this.transporter = nodemailer.createTransport({
-      host: host,
-      port: port,
-      secure: true,
-      auth: {
-        user: sender,
-        pass: password,
-      },
-    });
-    console.log('*** emailer is connected ...');
-  }
+const host = process.env.EMAIL_HOST;
+const port = process.env.EMAIL_PORT;
+const sender = process.env.EMAIL_SENDER;
+const password = process.env.EMAIL_PASSWORD;
+const transporter = nodemailer.createTransport({
+  host: host,
+  port: port,
+  secure: true,
+  auth: {
+    user: sender,
+    pass: password,
+  },
+});
 
-  send(recipient, pin, subject, text) {
-    const message = {
-      from: this.sender,
-      to: recipient,
-      subject: subject,
-      text: `${pin} ${text}`
-    };
-    this.transporter.sendMail(message, function(error, info) {
-      if (error) {
-        console.log(`*** emailer failed: ${error}`);
-        throw error;
-      } else {
-        console.log(`*** emailer sent: ${info.messageId} to: ${message.to}`);
-      }
-    });
-  }
+export default () => {
+  console.log('*** emailer is connected ...');
+}
+
+export function send(recipient, pin, subject, text) {
+  const message = {
+    from: this.sender,
+    to: recipient,
+    subject: subject,
+    text: `${pin} ${text}`
+  };
+  this.transporter.sendMail(message, function(error, info) {
+    if (error) {
+      console.log(`*** emailer failed: ${error}`);
+      throw error;
+    } else {
+      console.log(`*** emailer sent: ${info.messageId} to: ${message.to}`);
+    }
+  });
 }
