4dad9ff46a398c60d4a447b48ec1e9040768c4ac
objektwerks <objektwerks@runbox.com>
2022-08-21 15:08:40 -0400

    refactored work order title change listener

--- client/workorders.view.js
+++ client/workorders.view.js
@@ -166,11 +166,9 @@ export default () => {
 
   getById('workorder-title-id').addEventListener('change', (event) => {
     const number = getValueById('workorder-number-id');
-    if (number > 0) { // is work order valid?
-      const title = getValueById('workorder-title-id');
-      if (title.length >= 4) { // is title valid
-        getById(number).children[0].innerHTML = title; // set new title to corresponding work orders list item
-      }
+    const title = getValueById('workorder-title-id');
+    if (number > 0 && title.length >= 4) { // is work order and title valid?
+      getById(number).children[0].innerHTML = title; // set new title to corresponding work orders list item
     }
   }, false);
 
