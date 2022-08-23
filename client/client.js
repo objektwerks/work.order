937a8e268c251dc099e72e906c85604e14f3205b
objektwerks <objektwerks@runbox.com>
2022-08-23 08:38:42 -0400

    renamed login view to login form

--- client/client.js
+++ client/client.js
@@ -3,7 +3,7 @@ import menu from './menu.js';
 import model from './model.js';
 import fetcher from './fetcher.js';
 import registerview from './register.view.js';
-import loginview from './login.view.js';
+import loginview from './login.form.js';
 import workordersview from './workorders.view.js';
 import userview from './user.view.js';
 
