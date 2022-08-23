bff2dd7a19bef56c9e1d832b3e0e5e55271dfbda
objektwerks <objektwerks@runbox.com>
2022-08-23 08:40:45 -0400

    renamed workorders view to work orders form

--- client/client.js
+++ client/client.js
@@ -4,7 +4,7 @@ import model from './model.js';
 import fetcher from './fetcher.js';
 import registerview from './register.form.js';
 import loginview from './login.form.js';
-import workordersview from './workorders.view.js';
+import workordersview from './work.orders.form.js';
 import userview from './user.form.js';
 
 export default () => {
