d33b1a2338d938118df6a3b0777463dd4569f33b
objektwerks <objektwerks@runbox.com>
2022-08-27 18:43:52 -0400

    renamed variable in emailer

--- src/server/emailer.ts
+++ src/server/emailer.ts
@@ -3,7 +3,7 @@ import SMTPTransport from 'nodemailer/lib/smtp-transport'
 import { SentMessageInfo } from 'nodemailer/lib/sendmail-transport'
 
 const sender = process.env.WORK_ORDER_EMAIL_SENDER as string
-const config: SMTPTransport.Options = {
+const options: SMTPTransport.Options = {
   host: process.env.WORK_ORDER_EMAIL_HOST,
   port: parseInt( process.env.WORK_ORDER_EMAIL_PORT ?? '587' ),
   secure: true,
@@ -13,7 +13,7 @@ const config: SMTPTransport.Options = {
   },
   logger:true
 }
-const transporter: Transporter<SMTPTransport.SentMessageInfo> = nodemailer.createTransport(config)
+const transporter: Transporter<SMTPTransport.SentMessageInfo> = nodemailer.createTransport(options)
 
 export default () => {
   console.log('*** emailer connected ...')
