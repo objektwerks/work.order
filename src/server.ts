2a2a80302dec93de7e062b0bca78c371ff0d13b3
objektwerks <objektwerks@runbox.com>
2022-08-31 15:24:24 -0400

    edited server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
-import store from './server/store.js';
-import emailer from './server/emailer.js';
-import service from './server/service.js';
-import router from './server/router.js';
+import store from './server/store.js'
+import emailer from './server/emailer.js'
+import service from './server/service.js'
+import router from './server/router.js'
 
-store();
-emailer();
-service();
-router();
+store()
+emailer()
+service()
+router()
