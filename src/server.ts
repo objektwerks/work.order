d234687fc04a7071ad9f6f866218b49e88cfabcd
objektwerks <objektwerks@runbox.com>
2022-09-01 17:30:24 -0400

    added handler to server

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import store from './server/store.js'
 import emailer from './server/emailer.js'
 import service from './server/service.js'
+import handler from './server/handler.js'
 import router from './server/router.js'
 
 store()
 emailer()
 service()
+handler()
 router()
