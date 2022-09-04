459be4c83458b9fa2721cb57040a74e3ca0fbca4
objektwerks <objektwerks@runbox.com>
2022-09-04 14:38:17 -0400

    added logging to server

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,20 @@ import emailer from './server/emailer.js'
 import service from './server/service.js'
 import handler from './server/handler.js'
 import router from './server/router.js'
+import { logger } from './server/logger.js'
 
 store()
 emailer()
 service()
 handler()
 router()
+
+process.on('uncaughtException', (error) => {
+  logger.error(error, '*** server uncaught exception thrown. terminating server.')
+  process.exit(1)
+})
+
+process.on('unhandledRejection', (error) => {
+  logger.error(error, '*** server unhandled rejection thrown. terminating server.')
+  process.exit(1)
+})
