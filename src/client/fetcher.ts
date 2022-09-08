22d8dbc832ef65997b350352303fcec7de5f8f43
objektwerks <objektwerks@runbox.com>
2022-09-08 07:13:57 -0400

    edited fetcher

--- src/client/fetcher.ts
+++ src/client/fetcher.ts
@@ -50,8 +50,8 @@ function workOrderToFormData(workOrder: WorkOrder, imageFile: ImageFile[]): Form
   if (imageFile.length > 0) {
     const image = imageFile[0]
     workOrder.imageUrl = image.url
-    formData.append('imagefilename', image.filename)
     formData.append('image', image.file, image.filename)
+    formData.append('imagefilename', image.filename)
   }
   formData.append('workOrderAsJson', toJson(workOrder))
   return formData
