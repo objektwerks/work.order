b9143668789f289527fef98c954d64e24688832a
objektwerks <objektwerks@runbox.com>
2022-09-09 05:42:11 -0400

    added binder and role to client init

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,19 +1,23 @@
 import menu from './menu.js'
 import model from './model.js'
+import binder from './binder.js'
+import role from './role.js'
 import fetcher from './fetcher.js'
-import registerview from './register.form.js'
-import loginview from './login.form.js'
-import workordersview from './work.orders.form.js'
-import userview from './user.form.js'
+import registerForm from './register.form.js'
+import loginForm from './login.form.js'
+import workOrdersForm from './work.orders.form.js'
+import userForm from './user.form.js'
 
 export default () => {
   menu()
   model()
+  binder()
+  role()
   fetcher()
-  registerview()
-  loginview()
-  workordersview()
-  userview()
+  registerForm()
+  loginForm()
+  workOrdersForm()
+  userForm()
   console.log('*** client running ...')
   console.log('*** server url: ' + 'http://' + window.location.host)
 }
