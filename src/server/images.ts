444d037320e869a95685d3c44752353f92284ce5
objektwerks <objektwerks@runbox.com>
2022-09-10 08:58:33 -0400

    edited images logging

--- src/server/images.ts
+++ src/server/images.ts
@@ -33,11 +33,11 @@ const imagesDir = './images'
 function removeTxtFiles() {
   fs.readdir(imagesDir, (error, files) => {
     if (error) {
-      logger.error(`*** error reading images dir (${imagesDir}): `, error)
+      logger.error(`*** error reading images dir (${imagesDir}): ${error.message}`)
     } else {
       const txtFiles = files.filter(file => path.extname(file) === txt)
       for(const txtFile of txtFiles) fs.unlinkSync(txtFile)
-      logger.info('*** removed images txt files: ', txtFiles)
+      logger.info(`*** removed images txt files: ${txtFiles}`)
     }
   })
 }
