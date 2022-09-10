28066693a3baeac18ab207a405e0a956030f3fec
objektwerks <objektwerks@runbox.com>
2022-09-10 08:47:14 -0400

    enhanced emailer

--- src/server/emailer.ts
+++ src/server/emailer.ts
@@ -6,7 +6,8 @@ const sender = process.env.WORK_ORDER_EMAIL_SENDER as string
 const options: Mail.Options = {
   host: process.env.WORK_ORDER_EMAIL_HOST,
   port: parseInt( process.env.WORK_ORDER_EMAIL_PORT ?? '587' ),
-  secure: true,
+  secure: false,
+  requireTLS: true,
   auth: {
     user: sender,
     pass: process.env.WORK_ORDER_EMAIL_PASSWORD as string,
