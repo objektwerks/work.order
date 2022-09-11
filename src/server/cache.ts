d35fa44b3a6824dcc36a5341aea296c59cfb6a78
objektwerks <objektwerks@runbox.com>
2022-09-11 17:33:30 -0400

    enhanced cache

--- src/server/cache.ts
+++ src/server/cache.ts
@@ -1,4 +1,5 @@
 import * as store from './store.js'
+import { isLicense } from './entity.js'
 
 const licenseCache = new Set<string>()
 
@@ -24,7 +25,7 @@ export default () => {
 }
 
 export function addLicense(license: string): void {
-  if (!licenseCache.has(license) && license.length === 36) licenseCache.add(license)
+  if (!licenseCache.has(license) && isLicense(license)) licenseCache.add(license)
 }
 
 export function isLicenseValid(license: string): boolean {
