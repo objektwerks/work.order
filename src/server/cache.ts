efd2aeda8046bc91692ef257bddc054e3d18b9dd
objektwerks <objektwerks@runbox.com>
2022-09-12 06:34:18 -0400

    refactored cache

--- src/server/cache.ts
+++ src/server/cache.ts
@@ -1,21 +1,9 @@
-import * as store from './store.js'
 import { isLicense } from './entity.js'
 
 const licenseCache = new Set<string>()
 
-async function loadLicenseCache(): Promise<number> {
-  for ( const license of await store.listLicenses() ) {
-    licenseCache.add(license)
-  }
-  return licenseCache.size
-}
-
 export default () => {
   console.log('*** cache init ...')
-
-  loadLicenseCache().then(count => {
-    console.log('*** license cache count: ', count)
-  })
 }
 
 export function addLicense(license: string): void {
