f1b9425662e0bcf15c7a781ca6059281c6a8f684
objektwerks <objektwerks@runbox.com>
2022-09-15 18:04:46 -0400

    added dotenv to server

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+import * as dotenv from 'dotenv'
 import store from './server/store.js'
 import cache from './server/cache.js'
 import emailer from './server/emailer.js'
@@ -5,6 +6,8 @@ import service from './server/service.js'
 import handler from './server/handler.js'
 import images from './server/images.js'
 import router from './server/router.js'
+
+dotenv.config({path: './.env'})
 
 store()
 cache()
