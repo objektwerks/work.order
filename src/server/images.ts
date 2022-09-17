4e3321eb6f061e2789925e463d1cb2341e69c8f4
objektwerks <objektwerks@runbox.com>
2022-09-17 13:25:37 -0400

    adapted mime type function to images

--- src/server/images.ts
+++ src/server/images.ts
@@ -1,5 +1,6 @@
 import multer, { FileFilterCallback } from 'multer'
 import { Request } from "express"
+import { isMimeTypeAccepted } from './entity.js'
 
 type DestinationCallback = (error: Error | null, destination: string) => void
 type FileNameCallback = (error: Error | null, filename: string) => void
@@ -14,7 +15,7 @@ const storage = multer.diskStorage({
 })
 
 const fileFilter = (request: Request, file: Express.Multer.File, callback: FileFilterCallback) => {
-  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+  if (isMimeTypeAccepted(file.mimetype)) {
     // This filter is key to preventing the storage of temporary fake *.txt files
     // from being written to imagesDir. See client/fetcher for more details.
     callback(null, true)
