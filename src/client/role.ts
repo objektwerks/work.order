ace6ce65a77b9ed700f76e8cd760455fb5b7bd1f
objektwerks <objektwerks@runbox.com>
2022-09-20 11:25:38 -0400

    refactored role

--- src/client/role.ts
+++ src/client/role.ts
@@ -1,4 +1,4 @@
-import { disable, enable, removeReadonlyById, addReadonlyById, hide, show } from './dom.js'
+import { disable, enable, getValueById, removeReadonlyById, addReadonlyById, hide, show } from './dom.js'
 import { homeowner, serviceProvider } from './entity.js'
 
 export const readonlyRole = 'readonly'
@@ -11,9 +11,15 @@ export function apply(role: string) {
   if (role === homeowner) {
     // always readonly removeReadonlyById('workorder-number-id')
     // always readonly removeReadonlyById('workorder-homeowner-id')
-    removeReadonlyById('workorder-service-provider-id')
-    removeReadonlyById('workorder-title-id')
-    removeReadonlyById('workorder-issue-id')
+    if ( parseInt( getValueById('workorder-number-id') ) === 0 ) { // new work order
+      removeReadonlyById('workorder-service-provider-id')
+      removeReadonlyById('workorder-title-id')
+      removeReadonlyById('workorder-issue-id')
+    } else { // immutable work order
+      addReadonlyById('workorder-service-provider-id')
+      addReadonlyById('workorder-title-id')
+      addReadonlyById('workorder-issue-id')
+    }
     show('workorder-image-file-container-id')
     addReadonlyById('workorder-resolution-id')
     // always readonly addReadonlyById('workorder-opened-id')
