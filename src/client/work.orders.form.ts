8d9a96e8f610b5d73fed8457fca528084e999326
objektwerks <objektwerks@runbox.com>
2022-09-22 10:59:25 -0400

    removed import in work orders form

--- src/client/work.orders.form.ts
+++ src/client/work.orders.form.ts
@@ -2,7 +2,7 @@ import * as fetcher from './fetcher.js'
 import * as model from './model.js'
 import * as binder from './binder.js'
 import * as role from './role.js'
-import { getById, getFileById, getImageUrlById, getSelectedIndexId, getValueById, hide, show, setErrorsList, setValueById, setImageUrlById } from './dom.js'
+import { getById, getFileById, getImageUrlById, getSelectedIndexId, getValueById, hide, show, setErrorsList, setValueById } from './dom.js'
 import { getFileExtByMimeType, ListWorkOrders, validateWorkOrder, WorkOrder } from './entity.js'
 
 export default () => {
